03e3088d715a469e55b9bbbc7b7383cd3ca3c1b2
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2025-02-09 00:12:28 -0600

    Added authOptions library file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,50 +1,5 @@
-import { User } from "@/app/models/User"
-import mongoose from "mongoose"
 import NextAuth, { getServerSession } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import GoogleProvider from "next-auth/providers/google"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import client from "@/libs/mongoconnect"
-import { UserInfo } from "@/app/models/UserInfo"
-
-const bcrypt = require('bcrypt')
-
-export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(client),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }),
-    CredentialsProvider({
-        // The name to display on the sign in form (e.g. 'Sign in with...')
-        name: 'Credentials',
-        id: 'credentials',
-        // The credentials is used to generate a suitable form on the sign in page.
-        // You can specify whatever fields you are expecting to be submitted.
-        // e.g. domain, username, password, 2FA token, etc.
-        // You can pass any HTML attribute to the <input> tag through the object.
-        credentials: {
-          username: { label: "Email", type: "email", placeholder: "test@example.com" },
-          password: { label: "Password", type: "password" }
-        },
-        async authorize(credentials, req) {
-          const {email, password} = credentials
-
-          mongoose.connect(process.env.MONGO_URL)
-          const user = await User.findOne({email})
-          const passwordOk = user && bcrypt.compareSync(password, user.password)
-          if (passwordOk) {
-            return user
-          }
-    
-          // Return null if user data could not be retrieved
-          return null
-        }
-      })
-  ]
-}
+import {authOptions} from "@/libs/authOptions"
 
 const handler = NextAuth(authOptions)
 
---
+++ src/libs/authOptions.js
@@ -0,0 +1,36 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import client from "@/libs/mongoconnect";
+import { User } from "@/app/models/User";
+
+export const authOptions = {
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(client),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+      credentials: {
+        username: { label: "Email", type: "email", placeholder: "test@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+        await mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+
+        if (user && bcrypt.compareSync(password, user.password)) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+};
